96c6ee510f1ee540a93f34bda9b8aaffd375fdba
Lo-river <loriver.loof@gmail.com>
2025-11-13 14:39:53 +0100

    Allow CI to pass without tests yet

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/setupTest.ts"
+    setupFiles: "./src/setupTests.ts",
+    passWithNoTests: true,   
   }
 });
